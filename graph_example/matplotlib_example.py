79626f2be3964c6d7178da5135966c7dc2186b50
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-15 10:55:05 +0900

    [Modify] - Print the list of years and gdp

---
+++ graph_example/matplotlib_example.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+gdp = [300.2, 543.2, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+
+for i in years:
+    print(i)
+
+print("------------")
+
+for j in gdp:
+    print(j)
