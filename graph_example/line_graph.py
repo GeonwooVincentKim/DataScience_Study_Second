23660ba5e318f9c3508f4935f102e9265ff0f9d3
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-15 15:36:54 +0900

    [Feature] - Added Line-Graph file to check Line in the graph

---
+++ graph_example/line_graph.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+
+variance = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+bias_squared = [256, 128, 64, 32, 16, 8, 4, 2, 1]
+total_error = [x + y for x, y in zip(variance, bias_squared)]
+xs = [i for i, _ in enumerate(variance)]
+
+print(total_error)
+print(xs)
+
+plt.plot(xs, variance, 'g-', label='variance')
+plt.plot(xs, bias_squared, 'r-.', label='bias^2')
+plt.plot(xs, total_error, 'b:', label='total error')
+
+plt.legend(loc=9)
+plt.xlabel("Model Complexity")
+plt.xticks([])
+plt.title("The Bias-Variance Tradeoff")
+plt.show()
