6603add99902648a0748f1bcef73ee73c669d99e
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-16 15:26:12 +0900

    [Feature] - Identify Matrix example

--- linear_regression_example/matrix_example.py
+++ linear_regression_example/matrix_example.py
@@ -101,4 +101,25 @@ print("Check Identify-Matrix -> {0}".format(
         [0, 0, 0, 0, 1]
     ])
 )
-    
+
+
+friendships_pairs = [
+    (0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+    (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)
+]
+
+friends_of_five = [
+    i
+    for i, is_friend in enumerate(
+        # identify_matrix(5) == [
+        #     [1, 0, 0, 0, 0],
+        #     [0, 1, 0, 0, 0],
+        #     [0, 0, 1, 0, 0],
+        #     [0, 0, 0, 1, 0],
+        #     [0, 0, 0, 0, 1]
+        # ]
+        identify_matrix(5)
+    )
+    if is_friend
+]
+print(friends_of_five)
