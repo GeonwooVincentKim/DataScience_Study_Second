93ffa7d6817fcf5f296d1ac1fa5e364454c2dc52
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-16 15:18:24 +0900

    [Feature] - Check Identify-Matrix

--- linear_regression_example/matrix_example.py
+++ linear_regression_example/matrix_example.py
@@ -1,5 +1,5 @@
 from typing import List
-from typing import Tuple
+from typing import Tuple, Callable
 
 
 Matrix = List[List[float]]
@@ -31,14 +31,14 @@ for i in B:
     print("")
 
 
-def shape(A: Matrix) -> Tuple[int: int]:
-    """Return (Number of Rows, Number of Columns)"""
-    num_rows = len(A)
-    num_cols = len(A[0]) if A else 0
+# def shape(A: Matrix) -> Tuple[int: int]:
+#     """Return (Number of Rows, Number of Columns)"""
+#     num_rows = len(A)
+#     num_cols = len(A[0]) if A else 0
     
-    return num_rows, num_cols
+#     return num_rows, num_cols
 
-assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)
+# assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)
 
 
 def get_row(A: Matrix, i: int) -> Vector:
@@ -65,3 +65,40 @@ print(get_column(B, 0) == [1, 3, 5])
 # False Case
 print(get_column(B, 1) == [1, 2, 3])
 print(get_column(B, 0) == [2, 4, 6])
+
+
+def make_matrix(num_rows: int,
+                num_cols: int,
+                entry_fn: Callable[[int, int], float]) -> Matrix:
+    """
+    Return `num_rows * num_cols` List that the number of `(i, j)`
+    equals to `entry_fn(i, j)`
+    """
+    return [[entry_fn(i, j)
+             for j in range(num_cols)]
+            for i in range(num_rows)]
+    
+
+# make_matrix(1, 2, get_column((B, 1), j == (2, 4, 6)))
+    
+
+def identify_matrix(n: int) -> Matrix:
+    """Return `n * n` unit matrix"""
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
+
+
+# assert identify_matrix(5) == [[1, 0, 0, 0, 0],
+#                               [0, 1, 0, 0, 0],
+#                               [0, 0, 1, 0, 0],
+#                               [0, 0, 0, 1, 0],
+#                               [0, 0, 0, 0, 1]]
+print("Check Identify-Matrix -> {0}".format(
+    identify_matrix(5) == [
+        [1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1]
+    ])
+)
+    
