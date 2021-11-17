74f25b806b59b8ae5c8183538ea5848dd0edc42b
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-17 09:50:27 +0900

    [Update] - Testing

--- get_frequency_example.py
+++ get_frequency_example.py
@@ -8,8 +8,40 @@ for i in a:
         b.append(i)
         
     elif(i in b):
-        print(a, count)
-        count += 1
+        # print(a, count)
         many_frequency_number = i
+        count += 1
+        # print("Many Frequency Number : {0} - Count : {1}".format(many_frequency_number, count))
+        # many_frequency_number = i
+        # print("Many Frequency Number : {0} - Count : {1}".format(many_frequency_number, count))
         
+
+get_previous_frequency_number = 0
+get_next_frequency_number = 0
+for i in a:
+    for j in a:
+        get_previous_frequency_number = i
+        get_next_frequency_number = j
+        print(get_next_frequency_number)
+        
+        if(get_previous_frequency_number < get_next_frequency_number):
+        #     # print("Bigger")
+            temp_value = get_previous_frequency_number
+            get_previous_frequency_number = get_next_frequency_number
+            get_previous_frequency_number = temp_value
+        
+        #     many_frequency_number = get_previous_frequency_number
+        
+        # elif(get_previous_frequency_number < get_next_frequency_number):
+        #     many_frequency_number = get_next_frequency_number
+
+    
 print("Many Frequency Number : {0} - Count : {1}".format(many_frequency_number, count))
+# print("{0}".format())
+    
+
+# for i in b:
+#     # get_previous_frequency_number = many_frequency_number
+#     print("Many Frequency Number : {0} - Count : {1}".format(i, count))
+    
+
