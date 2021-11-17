9472ca485aef0eba50265abe64df31d1823ef87a
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-17 09:55:23 +0900

    [Update] - Delete not to use code

--- get_frequency_example.py
+++ get_frequency_example.py
@@ -16,25 +16,32 @@ for i in a:
         # print("Many Frequency Number : {0} - Count : {1}".format(many_frequency_number, count))
         
 
-get_previous_frequency_number = 0
-get_next_frequency_number = 0
-for i in a:
-    for j in a:
-        get_previous_frequency_number = i
-        get_next_frequency_number = j
-        print(get_next_frequency_number)
+# get_previous_frequency_number = 0
+# get_next_frequency_number = 0
+# temp_value = 0
+# for i in a:
+#     for j in a:
+#         get_previous_frequency_number = i
+#         get_next_frequency_number = j
+#         print(get_next_frequency_number)
         
-        if(get_previous_frequency_number < get_next_frequency_number):
-        #     # print("Bigger")
-            temp_value = get_previous_frequency_number
-            get_previous_frequency_number = get_next_frequency_number
-            get_previous_frequency_number = temp_value
+#         if(get_previous_frequency_number < get_next_frequency_number):
+#         #     # print("Bigger")
+#             temp_value = get_previous_frequency_number
+#             get_previous_frequency_number = get_next_frequency_number
+#             get_previous_frequency_number = temp_value
+#             print("temp Value : {0}".format(temp_value))
         
-        #     many_frequency_number = get_previous_frequency_number
+#         elif(get_previous_frequency_number > get_next_frequency_number and many_frequency_number == count):
+#             temp = get_next_frequency_number
+#             get_next_frequency_number = get_previous_frequency_number
+#             get_next_frequency_number = temp_value
+#             print("temp Value 23 : {0}".format(temp_value))
+#         #     many_frequency_number = get_previous_frequency_number
         
-        # elif(get_previous_frequency_number < get_next_frequency_number):
-        #     many_frequency_number = get_next_frequency_number
-
+#         # elif(get_previous_frequency_number < get_next_frequency_number):
+#         #     many_frequency_number = get_next_frequency_number
+    
     
 print("Many Frequency Number : {0} - Count : {1}".format(many_frequency_number, count))
 # print("{0}".format())
