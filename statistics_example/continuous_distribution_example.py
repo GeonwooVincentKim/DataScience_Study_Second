e2873ac46171dab11f0801a1ae67023c0a902986
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-19 13:08:48 +0900

    [Feature] - Show Matplotlib Graph

--- statistics_example/continuous_distribution_example.py
+++ statistics_example/continuous_distribution_example.py
@@ -1,9 +1,26 @@
+import math
 from matplotlib import pyplot as plt
+
+SQRT_TWO_PI = math.sqrt(2 * math.pi)
 
 
 def uniform_pdf(x: float):
     return 1 if 0 <= x < 1 else 0
 
 
-plt.plot(uniform_pdf(23), uniform_pdf(23))
+def uniform_cdf(x: float):
+    if x < 0: return 0
+    elif x < 1: return x
+    else : return 1
+
+
+def normal_pdf(x: float, mu: float = 0, sigma: float = 1):
+    return (math.exp(-(x - mu) ** 2 / 2 / sigma ** 2) / (SQRT_TWO_PI * sigma))
+
+
+xs = [x / 10.0 for x in range(-50, 50)]
+print(xs)
+
+plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label='mu=0, sigma=1')
+plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '--', label='mu=0, sigma=2')
 plt.show()
--- statistics_example/continuous_distribution_seaborn_example.py
+++ statistics_example/continuous_distribution_seaborn_example.py
@@ -3,3 +3,4 @@ import seaborn as sns
 df = sns.load_dataset('tips')
 print(df.head())
 
+print(sns.displot(df['tip']))
