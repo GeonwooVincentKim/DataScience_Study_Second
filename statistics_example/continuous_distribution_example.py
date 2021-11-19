f1e2a0226ed67c1a2c267f8d53592bf08de61d83
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-19 13:56:53 +0900

    [Feature] - Added Cumulative Distribution Function

--- statistics_example/continuous_distribution_example.py
+++ statistics_example/continuous_distribution_example.py
@@ -14,6 +14,7 @@ def uniform_cdf(x: float):
     else : return 1
 
 
+# Normal Distribution 
 def normal_pdf(x: float, mu: float = 0, sigma: float = 1):
     return (math.exp(-(x - mu) ** 2 / 2 / sigma ** 2) / (SQRT_TWO_PI * sigma))
 
@@ -23,4 +24,20 @@ print(xs)
 
 plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label='mu=0, sigma=1')
 plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '--', label='mu=0, sigma=2')
+plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], ':', label='mu=0, sigma=0.5')
+plt.plot(xs, [normal_pdf(x, mu=-1) for x in xs], '-.', label='mu=-1, sigma=1')
+plt.plot(xs, [normal_pdf(x, mu=1) for x in xs], '.-', label='mu=1, sigma=1')
+
+plt.legend()
+plt.title("Various Normal PDFs")
+plt.show()
+
+
+# Cumulative Distribution Function
+def normal_cdf(x:float, mu:float = 0, sigma: float = 1):
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+
+xs = [x / 10.0 for x in range(-50, 50)]
+plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label='mu=0, sigma=1')
 plt.show()
