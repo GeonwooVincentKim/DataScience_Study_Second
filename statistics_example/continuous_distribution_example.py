bf4ad67d44cb268106d8e4c92989150d7c380ca3
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-19 14:14:05 +0900

    [Feature] - Cumulative Distribution Function

--- statistics_example/continuous_distribution_example.py
+++ statistics_example/continuous_distribution_example.py
@@ -40,4 +40,14 @@ def normal_cdf(x:float, mu:float = 0, sigma: float = 1):
 
 xs = [x / 10.0 for x in range(-50, 50)]
 plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label='mu=0, sigma=1')
+plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], '--', label='mu=0, sigma=2')
+plt.plot(xs, [normal_cdf(x, sigma=0.5) for x in xs], ':', label='mu=0, sigma=0.5')
+plt.plot(xs, [normal_cdf(x, mu=-1) for x in xs], '-.', label='mu=-1, sigma=1')
+plt.plot(xs, [normal_cdf(x, mu=1) for x in xs], '.-', label='mu=1, sigma=1')
+
+plt.plot(xs, [normal_cdf(x, mu=-1, sigma=-1) for x in xs], '^', label='mu=-1, sigma=-1')
+plt.plot(xs, [normal_cdf(x, sigma=-1) for x in xs], '*', label='mu=1, sigma=-1')
+
+plt.legend(loc=4)
+plt.title("Various Normal CDFs")
 plt.show()
