6294ded4957c4156f3a2a1afa88fc588fe24a7fa
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-23 17:25:35 +0900

    [Feature] - Learn about example of flipping-coins-example

--- hypothesis_and_inference/flipping_coins_example.py
+++ hypothesis_and_inference/flipping_coins_example.py
@@ -1,5 +1,13 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.join(os.path.dirname(__file__)))))
+
 from typing import Tuple
 import math
+
+from statistics_example.continuous_distribution_example import normal_cdf
+
+normal_probability_below = normal_cdf
 
 
 def normal_approximation_to_binomial(n: int, p: float):
@@ -15,4 +23,22 @@ def normal_approximation_to_binomial(n: int, p: float):
     return mu, sigma
 
 
-print("{0}".format(normal_approximation_to_binomial(1000, 0.5)))
+def normal_probability_above(lo: float, mu: float=0, sigma: float=1):
+    """
+    Cumulative Distribution Function represents the probability 
+    that the Random-Variable smaller than some particular value 
+    """
+    return 1 - normal_cdf(lo, mu, sigma)
+
+
+def normal_probability_between(lo: float, hi: float, mu: float=0, sigma: float=1):
+    """
+    Probability the normal-distribution that follows `mu(average)` and `sigma(standard-deviation)`
+    exists between `lo` and `hi`
+    """
+    return normal_cdf(hi, mu, sigma) - normal_cdf(lo, mu, sigma)
+
+
+print("\n---------------------\n")
+print("Normal Approximation to binomial function -> {0}".format(normal_approximation_to_binomial(1000, 0.5)))
+print("Normal Probability Above -> {0}".format(normal_probability_above(0.95, 1000, 0.5)))
